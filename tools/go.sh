7def5bda738b77d0b78ca221886e8e9262787f06
wangtuo0820 <wangtuo0820@sina.cn>
2019-03-25 10:55:47 +0800

    update go.sh

--- tools/go.sh
+++ tools/go.sh
@@ -1,2 +1,3 @@
-python3 train_net.py --config-file="../configs/tuo/e2e_faster_rcnn_R_50_C4_1x_1_gpu_voc_roi_7.yaml" OUTPUT_DIR ./checkpoint/roi_7
-python3 train_net.py --config-file="../configs/tuo/e2e_faster_rcnn_R_50_C4_1x_1_gpu_voc.yaml" OUTPUT_DIR ./checkpoint/offical
+#python3 train_net.py --config-file="../configs/tuo/e2e_faster_rcnn_R_50_C4_1x_1_gpu_voc_roi_7.yaml" OUTPUT_DIR ./checkpoint/roi_7
+python3 train_net.py --config-file="../configs/tuo/e2e_faster_rcnn_R_50_FPN_1x.yaml" OUTPUT_DIR ./checkpoint/fpn
+python3 train_net.py --config-file="../configs/tuo/e2e_faster_rcnn_R_50_C4_1x_1_gpu_voc.yaml" OUTPUT_DIR ./checkpoint/c4
